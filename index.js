7085ab7d26cea9f6f651882225805757e2eb9b21
Cris Mihalache <me@f3rno.com>
2020-06-28 20:31:50 +0700

    refactor: polish

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const template = require('./lib')
 
 /**
- * Simple default template for `serve-markdown-it`, supporting multiple color
- * themes and configurable syntax highlighting.
+ * @external serve-markdown-it
+ * @see https://github.com/f3rno/serve-markdown-it
+ */
+
+/**
+ * Simple default template for {@link external:serve-markdown-it}, supporting
+ * multiple color themes and configurable syntax highlighting.
  *
  * @module serve-markdown-it-template-default
  * @license MIT
